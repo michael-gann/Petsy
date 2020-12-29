9afb9ebc61fbdafed2ff279e8e86fbdbcf02c8b8
ryoung7986 <ryoung7986@gmail.com>
2020-12-28 19:36:47 -0500

    working on navbar component, added navbar.css

--- python-project-starter/react-app/src/components/NavBar.js
+++ python-project-starter/react-app/src/components/NavBar.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css';
 
 const NavBar = ({ setAuthenticated }) => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="nav navbar-container">
+      <ul className="nav navbar-items">
+        <li className="nav navbar-item">
           <NavLink to="/" exact={true} activeClassName="active">
-            Home
+            Petsy
           </NavLink>
         </li>
-        <li>
+        <input type="text" placeholder="Search for anything"></input>
+        <li className="nav navbar-item">
           <NavLink to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
         </li>
-        <li>
+        <li className="nav navbar-item">
           <NavLink to="/sign-up" exact={true} activeClassName="active">
             Sign Up
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
       </ul>
+      <div class="dropdown">
+        <button class="dropbtn">You
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <LogoutButton setAuthenticated={setAuthenticated} />
+        </div>
+      </div>
     </nav>
   );
 }
