6326a1a1c301e59e8517db9ceedb9ba0130a609e
nappernick <nickfmatthews@gmail.com>
2020-12-28 19:04:58 -0500

    Working

--- python-project-starter/react-app/src/components/auth/LoginForm.js
+++ python-project-starter/react-app/src/components/auth/LoginForm.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import { login } from "../../services/auth";
+import Modal from 'react-modal';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
 
 const LoginForm = ({ authenticated, setAuthenticated }) => {
+  const history = useHistory()
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [modalIsOpen, setIsOpen] = useState(true)
 
+
+  //* Modal Functions
+  // function afterOpenModal() {
+  //   subtitle.style.color = '#f00';
+  // }
+
+  function closeModal() {
+    setIsOpen(false);
+    return history.push('/')
+  }
+
+
+  //* Login functions
   const onLogin = async (e) => {
     e.preventDefault();
     const user = await login(email, password);
@@ -31,34 +57,42 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type="submit">Login</button>
-      </div>
-    </form>
+    <Modal
+      isOpen={modalIsOpen}
+      // onAfterOpen={afterOpenModal}
+      onRequestClose={closeModal}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      <form onSubmit={onLogin}>
+        <div>
+          {errors.map((error) => (
+            <div>{error}</div>
+          ))}
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={updatePassword}
+          />
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </Modal>
   );
 };
 
--- python-project-starter/react-app/src/components/auth/SignUpForm.js
+++ python-project-starter/react-app/src/components/auth/SignUpForm.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from "react-router-dom";
 import { signUp } from '../../services/auth';
+import Modal from 'react-modal';
 
-const SignUpForm = ({authenticated, setAuthenticated}) => {
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+const SignUpForm = ({ authenticated, setAuthenticated }) => {
+  const history = useHistory()
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
 
+
+  //* Signup functions
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
