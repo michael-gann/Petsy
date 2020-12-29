c359d0e9fc4726a103230a7b44c6bde790d86276
ryoung7986 <ryoung7986@gmail.com>
2020-12-29 13:50:53 -0500

    added conditional render for authenticated users to navbar component

--- python-project-starter/react-app/src/components/NavBar.js
+++ python-project-starter/react-app/src/components/NavBar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import './NavBar.css';
 
-const NavBar = ({ setAuthenticated }) => {
+const NavBar = ({ setAuthenticated, isAuthenticated }) => {
   return (
     <nav className="nav navbar-container">
       <ul className="nav navbar-items">
@@ -13,25 +13,29 @@ const NavBar = ({ setAuthenticated }) => {
           </NavLink>
         </li>
         <input type="text" placeholder="Search for anything"></input>
-        <li className="nav navbar-item">
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
+        {!isAuthenticated &&
+          <>
+            <li className="nav navbar-item">
+              <NavLink to="/login" exact={true} activeClassName="active">
+                Login
           </NavLink>
-        </li>
-        <li className="nav navbar-item">
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
+            </li>
+            <li className="nav navbar-item">
+              <NavLink to="/sign-up" exact={true} activeClassName="active">
+                Sign Up
           </NavLink>
-        </li>
+            </li>
+          </>}
       </ul>
-      <div class="dropdown">
-        <button class="dropbtn">You
+      {isAuthenticated &&
+        <div class="dropdown">
+          <button class="dropbtn">You
           <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <LogoutButton setAuthenticated={setAuthenticated} />
-        </div>
-      </div>
+          </button>
+          <div class="dropdown-content">
+            <LogoutButton setAuthenticated={setAuthenticated} />
+          </div>
+        </div>}
     </nav>
   );
 }
