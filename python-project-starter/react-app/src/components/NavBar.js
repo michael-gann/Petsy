27eeb3603d07bfebe8598943cbb3537309c183e6
nappernick <nickfmatthews@gmail.com>
2020-12-29 16:24:04 -0500

    Working

--- python-project-starter/react-app/src/components/NavBar.js
+++ python-project-starter/react-app/src/components/NavBar.js
@@ -6,30 +6,44 @@ import './NavBar.css';
 const NavBar = ({ setAuthenticated }) => {
   return (
     <nav className="nav navbar-container">
-      <ul className="nav navbar-items">
-        <li className="nav navbar-item">
-          <NavLink to="/" exact={true} activeClassName="active">
-            Petsy
+      <div className="upper-section">
+        <ul className="nav navbar-items">
+          <li className="nav navbar-item">
+            <NavLink to="/" exact={true} activeClassName="active">
+              Petsy
           </NavLink>
-        </li>
-        <input type="text" placeholder="Search for anything"></input>
-        <li className="nav navbar-item">
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
+          </li>
+          <input type="text" placeholder="Search for anything"></input>
+          {!isAuthenticated &&
+            <>
+              <li className="nav navbar-item">
+                <NavLink to="/login" exact={true} activeClassName="active">
+                  Login
           </NavLink>
-        </li>
-        <li className="nav navbar-item">
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
+              </li>
+              <li className="nav navbar-item">
+                <NavLink to="/sign-up" exact={true} activeClassName="active">
+                  Sign Up
           </NavLink>
-        </li>
-      </ul>
-      <div class="dropdown">
-        <button class="dropbtn">You
+              </li>
+            </>}
+        </ul>
+        {isAuthenticated &&
+
+          <div class="dropdown">
+            <button class="dropbtn">You
           <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-          <LogoutButton setAuthenticated={setAuthenticated} />
+            </button>
+            <div class="dropdown-content"><LogoutButton setAuthenticated={setAuthenticated} />
+            </div>
+          </div>}
+      </div>
+      <div className="lower-section">
+        <div>
+          <NavLink to="/pets" exact={true}>Pets to Adopt</NavLink>
+        </div>
+        <div>
+          <NavLink to="/items" exact={true}>Items For Your Pets</NavLink>
         </div>
       </div>
     </nav>
