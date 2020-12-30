baad09875f37773149c5021a3d79f477a2caed77
nappernick <nickfmatthews@gmail.com>
2020-12-29 19:40:23 -0500

    Added items to homepage

--- python-project-starter/react-app/src/components/Homepage/Homepage.js
+++ python-project-starter/react-app/src/components/Homepage/Homepage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Pets from '../Pets/Pets'
+import Items from '../ItemsList'
 
 function Homepage() {
     return (
         <div className="page-container">
             <div className="pets">
                 <Pets />
+            </div>
+            <div className="items">
+                <Items />
             </div>
         </div>
     )
