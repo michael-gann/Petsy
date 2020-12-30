5a608bf29096fba16dea2b5f6caba6c5824cc608
pongdanny <pongdaniel17@gmail.com>
2020-12-30 11:16:44 -0500

    more seed data for reviews

--- python-project-starter/app/api/review_routes.py
+++ python-project-starter/app/api/review_routes.py
@@ -13,10 +13,5 @@ def reviews():
     reviews = Review.query.all()
     return {"Reviews": [review.to_dict() for review in reviews]}
 
-# @item_routes.route("/", methods=["GET"])
-# def items():
-#     items = db.session.query(Item).options(selectinload(Item.user)).all()
 
-#     items_with_seller = [{**item.to_dict(), "user": item.user.to_dict()} for item in items]
 
-#     return jsonify(items_with_seller)
