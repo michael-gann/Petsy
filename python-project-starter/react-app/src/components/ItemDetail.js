be3fa0e0608291dbeec687262eb9899f5cb3954d
Michael Gann <michael.gann@live.com>
2021-01-02 10:52:59 -0700

    fix review display errors if no reviews

--- python-project-starter/react-app/src/components/ItemDetail.js
+++ python-project-starter/react-app/src/components/ItemDetail.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import NumberFormat from 'react-number-format';
-import AddToCart from './ShoppingCart/AddToCart';
-import RemoveFromCart from './ShoppingCart/RemoveFromCart';
-import RenderReviews from './Reviews/RenderReviews'
-import ItemBySeller from "./MoreFromSeller/ItemBySeller"
-import PostReview from "./Reviews/PostReview"
-import ScoreAvg from "./Reviews/ScoreAvg"
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NumberFormat from "react-number-format";
+import AddToCart from "./ShoppingCart/AddToCart";
+import RemoveFromCart from "./ShoppingCart/RemoveFromCart";
+import RenderReviews from "./Reviews/RenderReviews";
+import ItemBySeller from "./MoreFromSeller/ItemBySeller";
+import PostReview from "./Reviews/PostReview";
+import ScoreAvg from "./Reviews/ScoreAvg";
 
 function ItemDetail({ user, isAuthenticated }) {
-  const [reviews, setReviews] = useState([])
+  const [reviews, setReviews] = useState([]);
   const [item, setItem] = useState([]);
-  const [seller, setSeller] = useState('');
+  const [seller, setSeller] = useState("");
   const { id } = useParams();
-  console.log(reviews)
+  console.log(reviews);
 
   // console.log("ID:", id)
 
@@ -22,17 +21,17 @@ function ItemDetail({ user, isAuthenticated }) {
     async function fetchData() {
       const response = await fetch(`/api/items/${id}`);
       const itemData = await response.json();
-      setItem(itemData)
+      setItem(itemData);
     }
     fetchData();
   }, [id]);
 
   useEffect(() => {
     async function fetchData() {
-      const sellerId = item.sellerId
+      const sellerId = item.sellerId;
       const response = await fetch(`/api/users/${sellerId}`);
       const sellerData = await response.json();
-      setSeller(sellerData)
+      setSeller(sellerData);
     }
     fetchData();
   }, [item]);
@@ -47,7 +46,9 @@ function ItemDetail({ user, isAuthenticated }) {
       <section>
         <div>
           <div>
-            <h6>{seller.firstName} {seller.lastName}</h6>
+            <h6>
+              {seller.firstName} {seller.lastName}
+            </h6>
           </div>
           <div>
             <ScoreAvg itemId={id} />
@@ -62,10 +63,10 @@ function ItemDetail({ user, isAuthenticated }) {
         <div>
           <NumberFormat
             value={item.price}
-            displayType={'text'}
+            displayType={"text"}
             thousandSeparator={true}
-            prefix={'$'}
-            renderText={value => <div>{value}</div>}
+            prefix={"$"}
+            renderText={(value) => <div>{value}</div>}
             decimalScale={2}
             fixedDecimalScale={true}
           />
@@ -76,14 +77,16 @@ function ItemDetail({ user, isAuthenticated }) {
         <RemoveFromCart id={item.id} />
         <div className="reviews">
           <RenderReviews reviews={reviews} setReviews={setReviews} />
-          {isAuthenticated && <PostReview user={user} reviews={reviews} setReviews={setReviews} />}
+          {isAuthenticated && (
+            <PostReview user={user} reviews={reviews} setReviews={setReviews} />
+          )}
         </div>
         <div>
           <ItemBySeller sellerId={item.sellerId} />
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default ItemDetail
+export default ItemDetail;
