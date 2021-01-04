a7299b131f2644cd86e83c557c948f9925ff234c
ryoung7986 <ryoung7986@gmail.com>
2021-01-03 22:48:06 -0500

    fixed cart styling/deleted old revision file

--- python-project-starter/react-app/src/components/ShoppingCart/Cart.js
+++ python-project-starter/react-app/src/components/ShoppingCart/Cart.js
@@ -19,67 +19,50 @@ function Cart() {
 
   const fetchItems = () => {
     return (
-      <ul>
+      <ul className="items-container">
         {cartItems.map((item) =>
-          <div key={item.id}>
-            <img src={item.imgurl} />
-            <div>
-              <NavLink to={`/items/${item.id}`}>{item.name}</NavLink>
-              <p>{`$${item.price}`}</p>
+          <div className="items-container__item" key={item.id}>
+            <img className="item-image" src={item.imgurl} />
+            <div className="item-info-container">
+              <div className="navlink">
+                <NavLink to={`/items/${item.id}`}><h1>{item.name}</h1></NavLink>
+              </div>
+              <div className="cart-page-button-container">
+                <RemoveItemFromCart item={item} setCartItems={setCartItems} />
+              </div>
             </div>
-            <select key={item.id} onChange={e => updateQty(e)}>
+            <select className="dropdown" key={item.id} onChange={e => updateQty(e)}>
               {loopArr.map(num => {
                 if (itemCarObj[item.id] == num) return <option id={`${item.id}|${num}`} selected='selected' value={`${item.id}|${num}`}>Qty: {num}</option>
                 else return <option id={`${item.id}|${num}`} value={`${item.id}|${num}`}>Qty: {num}</option>
               })}
-              {/* <option id={`${item.id}|1`} value={`${item.id}|1`}>Qty: 1</option> */}
-              {/* <option id={`${item.id}|2`} value={`${item.id}|2`}>Qty: 2</option>
-              <option id={`${item.id}|3`} value={`${item.id}|3`}>Qty: 3</option>
-              <option id={`${item.id}|4`} value={`${item.id}|4`}>Qty: 4</option>
-              <option id={`${item.id}|5`} value={`${item.id}|5`}>Qty: 5</option>
-              <option id={`${item.id}|6`} value={`${item.id}|6`}>Qty: 6</option>
-              <option id={`${item.id}|7`} value={`${item.id}|7`}>Qty: 7</option>
-              <option id={`${item.id}|8`} value={`${item.id}|8`}>Qty: 8</option>
-              <option id={`${item.id}|9`} value={`${item.id}|9`}>Qty: 9</option>
-              <option id={`${item.id}|10`} value={`${item.id}|10`}>Qty: 10</option> */}
             </select>
-            <div>
-              <RemoveItemFromCart item={item} setCartItems={setCartItems} />
-            </div>
-          )}
-        </ul>
-      </div>
+            <p>{`$${item.price}`}</p>
+          </div>
+        )}
+      </ul>
     )
   }
 
   const fetchPets = () => {
     return (
-<<<<<<< HEAD
       <div className="items-container">
         <ul>
           {localPetsCart !== null &&
             petsCart.map((pets) =>
               <div className="items-container__item" key={pets.id}>
-                <img src={pets.imgurl} />
+                <img className="item-image" src={pets.imgurl} />
                 <div className="item-info-container">
-                  <NavLink to={`/pets/${pets.id}`}><h1>{pets.name}</h1></NavLink>
+                  <div className="navlink">
+                    <NavLink to={`/pets/${pets.id}`}><h1>{pets.name}</h1></NavLink>
+                  </div>
                   <p>{pets.description}</p>
                   <div>
                     <RemovePetFromCart item={pets} setPetsCart={setPetsCart} />
                   </div>
                 </div>
-                <h6>{`$${pets.price}`}</h6>
-=======
-      <ul>
-                  {localPetsCart !== null &&
-                    petsCart.map((pets) =>
-                      <div key={pets.id}>
-                        <img src={pets.imgurl} />
-                        <div>
-                          <NavLink to={`/pets/${pets.id}`}>{pets.name}</NavLink>
-                          <p>{`$${pets.price}`}</p>
->>>>>>> master
-                        </div>
+                <p>{`$${pets.price}`}</p>
+              </div>
             )}
         </ul>
       </div>
@@ -87,23 +70,11 @@ function Cart() {
   }
 
   const updateQty = (e) => {
-                  let value = e.target.value.split("|")[1]
+    let value = e.target.value.split("|")[1]
     let itemId = e.target.value.split("|")[0]
     // console.log("Value:", value)
     // console.log("Item ID:", itemId)
 
-<<<<<<< HEAD
-    let targetItem = cartCopy.find(cartItem => {
-      return cartItem.id == e.target.value
-    });
-
-    // targetItem[e.target.name.toString()] = e.target.value
-
-    // setCart(cartCopy);
-
-    // let cartString = JSON.stringify(cartCopy);
-    // localStorage.setItem('cart', cartString);
-=======
     let cartCopy = [...cartItems]
 
     let targetItem = cartCopy.find(cartItem => {
@@ -115,7 +86,7 @@ function Cart() {
 
     localItemCart.forEach(item => {
       if (Object.keys(item)[0] == itemIdString) {
-          item[itemIdString] = parseInt(value)
+        item[itemIdString] = parseInt(value)
         let cartObj = itemCarObj
         cartObj[itemIdString] = parseInt(value)
         setItemCartObj(cartObj)
@@ -124,37 +95,36 @@ function Cart() {
 
     let newCart = JSON.stringify(localItemCart)
     localStorage.setItem('cart', newCart)
->>>>>>> master
   }
 
   useEffect(() => {
-          async function fetchData() {
-            let fetchedItems = []
-            let fetchedPets = []
-            if (localItemsCart) {
-              localItemsCart.map(async (cartItem) => {
-                const response = await fetch(`/api/items/${(Object.keys(cartItem))}`);
-                const itemData = await response.json();
-                fetchedItems.push(itemData)
-                setCartItems([...fetchedItems])
-              })
-            }
-            if (localPetsCart) {
-              localPetsCart.map(async (cartItem) => {
-                const response = await fetch(`/api/pets/${(Object.keys(cartItem))}`);
-                const itemData = await response.json();
-                fetchedPets.push(itemData)
-                setPetsCart([...fetchedPets])
-              })
-            }
-          }
+    async function fetchData() {
+      let fetchedItems = []
+      let fetchedPets = []
+      if (localItemsCart) {
+        localItemsCart.map(async (cartItem) => {
+          const response = await fetch(`/api/items/${(Object.keys(cartItem))}`);
+          const itemData = await response.json();
+          fetchedItems.push(itemData)
+          setCartItems([...fetchedItems])
+        })
+      }
+      if (localPetsCart) {
+        localPetsCart.map(async (cartItem) => {
+          const response = await fetch(`/api/pets/${(Object.keys(cartItem))}`);
+          const itemData = await response.json();
+          fetchedPets.push(itemData)
+          setPetsCart([...fetchedPets])
+        })
+      }
+    }
     fetchData();
 
     const buildItemCartObj = () => {
-          let cartObj = {}
+      let cartObj = {}
       localItemsCart.map(item => {
-          cartObj[Object.keys(item)[0]] = Object.values(item)[0]
-        })
+        cartObj[Object.keys(item)[0]] = Object.values(item)[0]
+      })
       setItemCartObj(cartObj)
     }
     buildItemCartObj()
@@ -163,26 +133,26 @@ function Cart() {
 
 
   return (
-        <div className="cart-components">
-          <div>
-            <div className="num-cart-items">
-              <h1>{localItemsCart ?
-                localItemsCart.length :
-                "0"} items in your cart</h1>
-            </div>
-            {fetchItems()}
-            <div className="num-cart-pets">
-              <h1>{localPetsCart ?
-                localPetsCart.length :
-                "0"} pets in your cart</h1>
-            </div>
-            {fetchPets()}
-          </div>
-          <div>
-            <ProceedToCheckout />
-          </div>
+    <div className="cart-components">
+      <div>
+        <div className="num-cart-items">
+          <h1>{localItemsCart ?
+            localItemsCart.length :
+            "0"} items in your cart</h1>
         </div>
+        {fetchItems()}
+        <div className="num-cart-pets">
+          <h1>{localPetsCart ?
+            localPetsCart.length :
+            "0"} pets in your cart</h1>
+        </div>
+        {fetchPets()}
+      </div>
+      <div>
+        <ProceedToCheckout />
+      </div>
+    </div>
   )
 }
 
-export default Cart
+export default Cart;
